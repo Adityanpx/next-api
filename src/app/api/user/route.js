7008c7905f9d665b115a1cc7ce72e0c99227a1d8
Aditya Gavali <adityagavali0911@gmail.com>
2024-08-03 22:20:33 +0530

    learned POST request

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -5,3 +5,24 @@ export function GET(request){
     const data = user;
     return NextResponse.json(data,{status:200})
 }
+
+// export async function POST(request){
+//     let payload = await request.json();
+//     console.log(payload.name)
+//     if(!payload.name){
+//         return NextResponse.json({result:"require field not found"})
+// }
+     
+//     return NextResponse.json({result:"hello"})
+// }
+
+export async function POST(request){
+    let payload = await request.json()
+    console.log(request)
+    if(!payload.name || !payload.age || !payload.email){
+                 return NextResponse.json({result:"require field not found",success:false},{status:400})
+ }
+
+    return NextResponse.json({result:"Hello....I'm Aditya Gavali",success:true},{status:200})
+
+}
